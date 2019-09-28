bf0007c8647bfd6947b8dd1e09ca03c193ff8801
Sameer Ingavale <52799932+sameer-ingavale@users.noreply.github.com>
2019-09-28 17:39:56 +0530

    favicon add

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-let express = require('express');
-let parser = require('ua-parser-js');
-let favicon = require('serve-favicon');
-let path = require('path');
+let express = require("express");
+let parser = require("ua-parser-js");
+let favicon = require("serve-favicon");
+let path = require("path");
 
-const app = module.exports = express();
+const app = (module.exports = express());
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
-app.get('/api/whoami', (req,res) => {
+app.get("/api/whoami", (req, res) => {
+  let software = parser(req.get("user-agent"));
 
-    let software = parser(req.get('user-agent'));
+  let data = {
+    IP:
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress,
+    Language: req.acceptsLanguages()[0],
+    OS: software.os.name + " " + software.os.version
+  };
+  res.send(data);
+});
 
-    let data = {
-        IP: req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress,
-        Language: req.acceptsLanguages()[0],
-        OS: software.os.name + " " + software.os.version
-    }
-    res.send(data)
-})
+app.use(express.static("public"));
 
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/views/index.html`)
-})
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
 
 const portnumber = process.env.PORT || 8000;
 
-app.listen(portnumber);
+app.listen(portnumber, () => {
+  console.log(`listening on port ${portnumber}`);
+});
